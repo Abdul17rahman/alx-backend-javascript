b319ad163e3ba302d20f13dd2273b24d4f60efb3
Abdul Rahman <abdulnsamba@gmail.com>
2024-03-05 12:46:46 +0300

    empl

--- 0x00-ES6_basic/11-createEmployeesObject.js
+++ 0x00-ES6_basic/11-createEmployeesObject.js
@@ -1,9 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-  const emps = [];
-  for (const emp of employees) {
-    emps.push(emp);
-  }
   return {
-    [departmentName]: emps,
+    [departmentName]: [...employees],
   };
 }
