d3e5faeed0f87768d0edb9a8ebfc8112f6398b72
Abdul Rahman <abdulnsamba@gmail.com>
2024-03-06 17:52:25 +0300

    improved1

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { uploadPhoto, createUser } from "./utils"
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-	Promise.all([uploadPhoto(), createUser()])
-	.then(([photoData, userData]) => {
-		console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`)
-	}).catch(() => {
-		console.log('Signup system offline')
-	})
+  Promise.all([uploadPhoto(), createUser()])
+    .then(([photoData, userData]) => {
+      console.log(`${photoData.body} ${userData.firstName} ${userData.lastName}`);
+    }).catch(() => {
+      console.log('Signup system offline');
+    });
 }
