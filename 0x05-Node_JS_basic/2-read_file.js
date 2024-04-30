4b3bf5ff23acd4b178b6cd9d26566ebd817cf199
Abdul Rahman <abdulnsamba@gmail.com>
2024-04-30 22:34:27 +0300

    count

---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+
+function countStudents(filePath) {
+  try {
+    const data = fs.readFileSync(filePath, 'utf8');
+
+    // Lines
+    const lines = data.trim().split('\n');
+    console.log(`Number of students: ${lines.length - 1}`);
+    let countCs = 0;
+    let countSwe = 0;
+    const csStudents = [];
+    const sweStudents = [];
+    for (let i = 1; i < lines.length; i + 1) {
+      const val = lines[i].split(',');
+      const name = val[0];
+      const dep = val[3].trim();
+      if (dep === 'CS') {
+        countCs += 1;
+        csStudents.push(name);
+      }
+      if (dep === 'SWE') {
+        countSwe += 1;
+        sweStudents.push(name);
+      }
+    }
+    console.log(`Number of students in CS: ${countCs}. List: ${csStudents.join(', ')}`);
+    console.log(`Number of students in SWE: ${countSwe}. List: ${sweStudents.join(', ')}`);
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
