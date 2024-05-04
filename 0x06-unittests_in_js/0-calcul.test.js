2c60cfd14e422b284a55bf2d5312c5f3dc8b90f7
Abdul Rahman <abdulnsamba@gmail.com>
2024-05-04 17:11:33 +0300

    update

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -2,7 +2,25 @@ const { calculateNumber } = require('./0-calcul.js')
 const assert = require('assert')
 
 describe('Testing the addition function', () => {
-	it('Checking addition for int', () => {
+	it('normal addition', () => {
 		assert.strictEqual(calculateNumber(1,2), 3)
 	})
+	it('with decimal >= 0.5 ', () => {
+    assert.strictEqual(calculateNumber(1.5, 1), 3);
+    assert.strictEqual(calculateNumber(1, 2.6), 4);
+    assert.strictEqual(calculateNumber(1.5, 2.6), 5);
+    assert.strictEqual(calculateNumber(1.4, 2.6), 4);
+  });
+
+  it('decimal on one arg >= 0.5 ', () => {
+    assert.strictEqual(calculateNumber(1.4, 1), 2);
+    assert.strictEqual(calculateNumber(1, 2.1), 3);
+    assert.strictEqual(calculateNumber(1.4, 1.4), 2);
+  });
+
+  it('not NaN', () => {
+    assert.strictEqual(calculateNumber(), NaN);
+    assert.strictEqual(calculateNumber(1, 'e'), NaN);
+    assert.strictEqual(calculateNumber('e', 'e'), NaN);
+  });
 })
